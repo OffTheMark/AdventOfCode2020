097ea76e1f53891b39d6cf3487dadaf02b9c89d0
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2020-12-01 12:48:17 -0500

    Solve day 1 part 2

--- Day1/main.swift
+++ Day1/main.swift
@@ -20,11 +20,26 @@ struct Day1: DayCommand {
         let part1Solution = part1(with: numbers)
         printTitle("Part 1", level: .title1)
         print("Product:", part1Solution, terminator: "\n\n")
+        
+        let part2Solution = part2(with: numbers)
+        printTitle("Part 2", level: .title1)
+        print("Product:", part2Solution)
     }
     
     func part1(with numbers: [Int]) -> Int {
-        let combinationsOf2 = numbers.combinations(ofCount: 2)
-        let correctCombination = combinationsOf2.first(where: { combination in
+        let combinations = numbers.combinations(ofCount: 2)
+        let correctCombination = combinations.first(where: { combination in
+            return combination.reduce(0, +) == 2020
+        })!
+        
+        return correctCombination.reduce(into: 1, { product, number in
+            product *= number
+        })
+    }
+    
+    func part2(with numbers: [Int]) -> Int {
+        let combinations = numbers.combinations(ofCount: 3)
+        let correctCombination = combinations.first(where: { combination in
             return combination.reduce(0, +) == 2020
         })!
         
