1de44d3e67f4ad9912686c19059bd50742fd71a9
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2020-12-01 12:50:13 -0500

    Create Day 2 skeleton

---
+++ Day2/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Day2
+//
+//  Created by Marc-Antoine Malépart on 2020-12-01.
+//
+
+import Foundation
+import AdventOfCodeUtilities
+import ArgumentParser
+
+struct Day2: DayCommand {
+    @Argument(help: "Puzzle input path")
+    var puzzleInputPath: String
+    
+    func run() throws {
+        #warning("TODO: Part 2")
+    }
+}
+
+Day2.main()
