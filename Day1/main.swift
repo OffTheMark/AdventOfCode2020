490c16e570c66710d616ea713e8eef648d383a4e
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2020-12-01 09:06:44 -0500

    Solve day 1 part 1

--- Day1/main.swift
+++ Day1/main.swift
@@ -8,15 +8,29 @@
 import Foundation
 import AdventOfCodeUtilities
 import ArgumentParser
+import Algorithms
 
 struct Day1: DayCommand {
     @Argument(help: "Puzzle input path")
     var puzzleInputPath: String
     
     func run() throws {
-        let input = try readFile()
+        let numbers = try readLines().compactMap({ Int($0) })
         
-        #warning("TODO: Part 1")
+        let part1Solution = part1(with: numbers)
+        printTitle("Part 1", level: .title1)
+        print("Product:", part1Solution, terminator: "\n\n")
+    }
+    
+    func part1(with numbers: [Int]) -> Int {
+        let combinationsOf2 = numbers.combinations(ofCount: 2)
+        let correctCombination = combinationsOf2.first(where: { combination in
+            return combination.reduce(0, +) == 2020
+        })!
+        
+        return correctCombination.reduce(into: 1, { product, number in
+            product *= number
+        })
     }
 }
 
