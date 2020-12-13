d075d1b519d3b6417d3a99687509397cb0bd9fd9
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2020-12-13 11:24:33 -0500

    Solve day 13 part 2
    Based on https://www.reddit.com/r/adventofcode/comments/kc4njx/2020_day_13_solutions/gfobnvf

--- Day13/main.swift
+++ Day13/main.swift
@@ -9,6 +9,11 @@ import Foundation
 import AdventOfCodeUtilities
 import ArgumentParser
 
+enum Bus {
+    case inService(identifier: Int64)
+    case any
+}
+
 struct Day13: DayCommand {
     @Argument(help: "Puzzle input path")
     var puzzleInputPath: String
@@ -17,10 +22,21 @@ struct Day13: DayCommand {
         let lines = try readLines()
         let earliestDeparture = Int(lines[0])!
         let availableBuses = lines[1].components(separatedBy: ",").compactMap({ Int($0) })
+        let buses: [Bus] = lines[1].components(separatedBy: ",").map({ part in
+            if let identifier = Int64(part) {
+                return .inService(identifier: identifier)
+            }
+            
+            return .any
+        })
         
         let part1Solution = part1(earliestDeparture: earliestDeparture, availableBuses: availableBuses)
         printTitle("Title 1", level: .title1)
-        print("Result:", part1Solution)
+        print("Result:", part1Solution, terminator: "\n\n")
+        
+        let part2Solution = part2(using: buses)
+        printTitle("Title 2", level: .title1)
+        print("Result:", part2Solution)
     }
     
     func part1(earliestDeparture: Int, availableBuses: [Int]) -> Int {
@@ -40,6 +56,36 @@ struct Day13: DayCommand {
             })!
         
         return busAndEarliestDeparture.key * (busAndEarliestDeparture.value - earliestDeparture)
+    }
+    
+    func part2(using buses: [Bus]) -> Int64 {
+        let identifiers: [Int64] = buses.map({ bus in
+            switch bus {
+            case .inService(let identifier):
+                return identifier
+                
+            case .any:
+                return 1
+            }
+        })
+        
+        var increment = identifiers.first!
+        var index = 1
+        var timestamp = increment
+        
+        while index < identifiers.endIndex {
+            let identifier = identifiers[index]
+            
+            if (timestamp + Int64(index)).isMultiple(of: identifier) {
+                increment *= identifier
+                index += 1
+            }
+            else {
+                timestamp += increment
+            }
+        }
+        
+        return timestamp
     }
 }
 
